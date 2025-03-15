8bc7d5eccebe296f9de7498ef814e7bb2e1593ed
Cosmo <152229650+cosmoharper@users.noreply.github.com>
2025-03-15 11:14:51 -0400

    Attempt 2

--- lab_ticket_price_calculator/ticket_calculator.py
+++ lab_ticket_price_calculator/ticket_calculator.py
@@ -3,7 +3,7 @@ movie_type = input("What type of movie do you want to see (regular/premium)? ")
 
 def ticket_price(age, movie_type):
     if age <= 12:
-        return 8 if movie_type == "regular" else 12
+        return 8 if movie_type == "regular" else 10
     elif age in range(13, 60):
         return 12 if movie_type == "regular" else 15
     else:  # age > 60
